fa1324426860e3aff14582f7cb2fbca4c23b4f01
Rashi Gupta <rashig2344@gmail.com>
2026-06-29 17:12:31 +0530

    new changes

--- src/components/DemoSection.jsx
+++ src/components/DemoSection.jsx
@@ -2,9 +2,9 @@ import githubDashboard from "../assets/github_dashboard.png";
 
 export default function DemoSection() {
   return (
-    <section className="-mt-25 px-6 lg:px-8 mb-40">
+    <section className="mt-10 sm:-mt-25 px-4 sm:px-6 lg:px-8 mb-40">
       <div
-        className="ml-30 mx-auto w-305 rounded-2xl bg-zinc-950 p-3 sm:p-5 ring-1 ring-zinc-800/60"
+        className="w-full max-w-5xl mx-auto rounded-2xl bg-zinc-950 p-3 sm:p-5 ring-1 ring-zinc-800/60"
         style={{
           backgroundImage:
             "radial-gradient(circle at 1px 1px, rgba(63,63,70,0.3) 1px, transparent 0)",
